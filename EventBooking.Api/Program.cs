ba70e0b2c50db3e85186c8c602bddc972bad2a2d
Amalanilkumar282 <amalanilkumar282@gmail.com>
2025-11-17 09:49:19 +0530

    swagger

--- EventBooking.Api/Program.cs
+++ EventBooking.Api/Program.cs
@@ -1,18 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Add OpenAPI / Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Swashbuckle middleware
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "EventBooking API V1");
+        c.RoutePrefix = "openapi/ui"; // serve UI at /openapi/ui
+    });
 }
+
+// keep MapOpenApi if still desired
+app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
